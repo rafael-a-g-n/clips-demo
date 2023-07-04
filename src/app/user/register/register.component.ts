7b9ae42485f45cf6e49d70a35ba04c4973c87df0
Rwedoll <ragn.ryzen@gmail.com>
2023-07-04 15:48:35 +0100

    Added custom Validators for confirming password and email taken check

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 
 @Component({
@@ -12,7 +14,8 @@ import IUser from 'src/app/models/user.model';
 export class RegisterComponent {
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private emailTaken: EmailTaken
   ) { }
 
   inSubmission = false
@@ -24,7 +27,7 @@ export class RegisterComponent {
   email = new FormControl('', [
     Validators.required,
     Validators.email
-  ])
+  ], [this.emailTaken.validate])
   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
@@ -54,7 +57,7 @@ export class RegisterComponent {
     password: this.password,
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber
-  })
+  }, [RegisterValidators.match('password', 'confirm_password')])
 
   async register() {
     this.showAlert = true
