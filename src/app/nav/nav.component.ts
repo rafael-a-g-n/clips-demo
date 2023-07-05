1a93ae44fb9315075837ac9194c4f9c60db405fd
Rwedoll <ragn.ryzen@gmail.com>
2023-07-05 15:35:27 +0100

    Added routing with forced redirection on logout of manageable content page

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -12,8 +11,7 @@ export class NavComponent implements OnInit {
 
   constructor(
     public modal: ModalService,
-    public auth: AuthService,
-    public afAuth: AngularFireAuth
+    public auth: AuthService
   ) { }
 
   ngOnInit(): void {
@@ -23,11 +21,5 @@ export class NavComponent implements OnInit {
     $event.preventDefault()
 
     this.modal.toggleModal('auth')
-  }
-
-  async logout($event: Event) {
-    $event.preventDefault()
-
-    await this.afAuth.signOut()
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,29 +2,41 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
-import { Observable } from 'rxjs';
-import { delay, map } from 'rxjs/operators'
+import { Observable, of } from 'rxjs';
+import { delay, map, filter, switchMap } from 'rxjs/operators'
+import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private usersCollection: AngularFirestoreCollection<IUser>
-  public isAuthenticated$: Observable<boolean>
-  public isAuthenticatedWithDelay$: Observable<boolean>
+  private usersCollection: AngularFirestoreCollection<IUser>;
+  public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
+  public redirect = false;
 
   constructor(
     private auth: AngularFireAuth,
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.usersCollection = db.collection('users')
     this.isAuthenticated$ = auth.user.pipe(
       map(user => !!user)
     )
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
-      delay(2000)
+      delay(1500)
     )
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => this.route.firstChild),
+      switchMap(route => route?.data ?? of({ authOnly: false }))
+    ).subscribe((data) => {
+      this.redirect = data.authOnly ?? false;
+    })
   }
 
   public async createUser(userData: IUser) {
@@ -50,5 +62,17 @@ export class AuthService {
     await userCred.user.updateProfile({
       displayName: userData.name
     })
+  }
+
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault()
+    }
+
+    await this.auth.signOut()
+
+    if (this.redirect) {
+      await this.router.navigateByUrl('/')
+    }
   }
 }
