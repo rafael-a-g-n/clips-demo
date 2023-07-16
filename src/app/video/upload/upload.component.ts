aa8c18037cfd3e20a6a8ec7404be7540a7cc7339
Rwedoll <ragn.ryzen@gmail.com>
2023-07-16 08:27:55 +0100

    Created file type protection and form visibility toggle on accepting of file upload.

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
   isDragOver = false
+  file: File | null = null
+  nextStep = false
+
   constructor() { }
 
   ngOnInit(): void {
@@ -14,5 +17,13 @@ export class UploadComponent implements OnInit {
 
   storeFile($event: Event) {
     this.isDragOver = false
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+
+    this.nextStep = true
   }
 }
