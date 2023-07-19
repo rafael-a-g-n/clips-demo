7f51ab495479d43d9249beac3d0a7f5261966e8b
Rwedoll <ragn.ryzen@gmail.com>
2023-07-19 18:16:14 +0100

    Updated from to use ReactiveForms and added the logic to the title of the file.

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -9,6 +10,18 @@ export class UploadComponent implements OnInit {
   isDragOver = false
   file: File | null = null
   nextStep = false
+
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  })
+
+  uploadForm = new FormGroup({
+    title: this.title
+  })
 
   constructor() { }
 
@@ -23,7 +36,13 @@ export class UploadComponent implements OnInit {
     if (!this.file || this.file.type !== 'video/mp4') {
       return
     }
-
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
     this.nextStep = true
+  }
+
+  uploadFile() {
+    console.log('File Uploaded');
   }
 }
