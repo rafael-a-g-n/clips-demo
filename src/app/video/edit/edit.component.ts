e3d03a30c6eaeb27a3c2cef2badbaf4c217b371f
Rwedoll <ragn.ryzen@gmail.com>
2023-07-20 15:25:57 +0100

    Added the logic to update the clip on submission

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-edit',
@@ -10,6 +11,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class EditComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null
+  inSubmission = false
+  showAlert = false
+  alertColor = 'blue'
+  alertMsg = 'Please wait! Updating clipt.'
 
   clipID = new FormControl('', {
     nonNullable: true
@@ -28,7 +33,7 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
     id: this.clipID
   })
 
-  constructor(private modal: ModalService) { }
+  constructor(private modal: ModalService, private clipService: ClipService) { }
 
   ngOnInit(): void {
     this.modal.register('editClip')
@@ -45,5 +50,29 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
 
     this.clipID.setValue(this.activeClip.docID ?? '')
     this.title.setValue(this.activeClip.title)
+  }
+
+  async submit() {
+    this.inSubmission = true
+    this.showAlert = true
+    this.alertColor = 'blue'
+    this.alertMsg = 'Please wait! Updating clip.'
+
+    try {
+      await this.clipService.updateClip(
+        this.clipID.value, this.title.value
+      )
+    }
+    catch (e) {
+      this.inSubmission = false
+      this.alertColor = 'red'
+      this.alertMsg = 'Something went wrong. Try again later'
+
+      return
+    }
+
+    this.inSubmission = false
+    this.alertColor = 'green'
+    this.alertMsg = 'Success!'
   }
 }
