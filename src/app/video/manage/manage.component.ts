f8d616869b7d5c5ed42a3e92a51f4fd2298a2733
Rwedoll <ragn.ryzen@gmail.com>
2023-07-20 11:01:47 +0100

    Implemented modal to edit clip

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -15,7 +16,8 @@ export class ManageComponent {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modal: ModalService
   ) { }
 
   ngOnInit(): void {
@@ -43,5 +45,10 @@ export class ManageComponent {
         sort: value
       }
     })
+  }
+
+  openModal($event: Event, clio: IClip) {
+    $event.preventDefault()
+    this.modal.toggleModal('editClip')
   }
 }
