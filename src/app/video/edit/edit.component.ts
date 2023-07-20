4f508c49237e6258f4e6f32b9edce152a27c7ec8
Rwedoll <ragn.ryzen@gmail.com>
2023-07-20 11:38:15 +0100

    Bound the edit form using reactive forms

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy {
+export class EditComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null
+
+  clipID = new FormControl('', {
+    nonNullable: true
+  })
+
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  })
+
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID
+  })
 
   constructor(private modal: ModalService) { }
 
@@ -18,5 +36,14 @@ export class EditComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.modal.unregister('editClip')
+  }
+
+  ngOnChanges() {
+    if (!this.activeClip) {
+      return
+    }
+
+    this.clipID.setValue(this.activeClip.docID ?? '')
+    this.title.setValue(this.activeClip.title)
   }
 }
