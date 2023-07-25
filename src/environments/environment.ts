a828c48c117c5815d77923e0a3250a52654dd3c2
Rwedoll <ragn.ryzen@gmail.com>
2023-07-25 17:45:20 +0100

    Added environments file to repo

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBht9ajWF3LN5uPL0brX3-USR3FxNhBLPg",
+    authDomain: "twatch-c02df.firebaseapp.com",
+    projectId: "twatch-c02df",
+    storageBucket: "twatch-c02df.appspot.com",
+    appId: "1:752613596831:web:9eb785479193b93ef04caa"
+  }
+};
