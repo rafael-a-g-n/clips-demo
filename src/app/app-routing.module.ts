ffff639ad83c4243e8a28101d70a636e235e912c
Rwedoll <ragn.ryzen@gmail.com>
2023-07-25 16:20:06 +0100

    Implemented lazy loading of video module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,10 @@ const routes: Routes = [
     }
   },
   {
+    path: '',
+    loadChildren: async () => (await import('./video/video.module')).VideoModule
+  },
+  {
     path: '**',
     component: NotFoundComponent
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth'
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
-import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
@@ -34,7 +33,6 @@ import { FbTimestampPipe } from './pipes/fb-timestamp.pipe'
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireStorageModule,
-    VideoModule,
     AppRoutingModule,
     AngularFireStorageModule
   ],
