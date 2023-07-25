093f6ddd51de3453dddd264447beab01106d025a
Rwedoll <ragn.ryzen@gmail.com>
2023-07-25 11:59:16 +0100

    Implemented scroll to the bottom of the page detection.

--- src/app/clipslist/clipslist.component.ts
+++ src/app/clipslist/clipslist.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clipslist',
   templateUrl: './clipslist.component.html',
   styleUrls: ['./clipslist.component.css']
 })
-export class ClipslistComponent {
+export class ClipslistComponent implements OnInit, OnDestroy {
+  constructor() { }
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight } = document.documentElement
+    const { innerHeight } = window
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+
+    if (bottomOfWindow) {
+      console.log('bottom of window');
+    }
+  }
 }
