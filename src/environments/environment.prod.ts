73b063e31eee67a2c3ce40e504f0cc814204412d
rafael-a-g-n <ragn.ryzen@gmail.com>
2026-05-09 20:44:01 +0100

    fix: update Clerk publishable key to live version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  clerkPublishableKey: 'pk_test_YWRhcHRpbmctaGF3ay0yMy5jbGVyay5hY2NvdW50cy5kZXYk',
+  clerkPublishableKey: 'pk_live_Y2xlcmsuY2xpcHMtZGVtby5wYWdlcy5kZXYk',
   apiUrl: 'https://clips-api.ragn.workers.dev',
   r2PublicUrl: 'https://pub-32cdd46afc054974a62ae644bc2977e2.r2.dev',
 };
